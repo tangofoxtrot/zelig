374c5f909195a128e475085b77941cdbbf2301ae
Jeremy <jeremy@sidereel.com>
2012-02-09 00:14:30 -0800

    helper methods for mocking a route's response

--- lib/zelig/mock_route.rb
+++ lib/zelig/mock_route.rb
@@ -6,5 +6,35 @@ module Zelig
       @route = route
       @response = {}
     end
+
+    def respond_with status, content, options = {}
+      description = options[:description] || description_from(status, route)
+
+      response[status] = {
+        'fixture_path' => write_fixture(status, content),
+        'description' => description
+      }
+
+      response['default'] = response[status] if options[:default]
+    end
+
+    private
+
+    def write_fixture status, content
+      fixture_path = File.join Zelig.fixture_dir, path_from(status, route)
+      File.open(fixture_path, "w") do |file|
+        file.write content
+      end
+      fixture_path.to_s
+    end
+
+    def path_from status, route
+      path = route.gsub(/:/, '').gsub(/[^[:alnum:]]/, '_')
+      "#{status}#{path}.fixture"
+    end
+
+    def description_from status, route
+      "#{status} response for #{route}"
+    end
   end
 end
---
+++ spec/lib/zelig/mock_route_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+require 'lib/zelig'
+
+describe Zelig::MockRoute do
+  let(:mock_route) { Zelig::MockRoute.new("/links/:id") }
+
+  before do
+    Zelig.configure { |config|  config.output_dir = "test_dir" }
+  end
+
+  describe "mocking a routes response" do
+    context "by default" do
+      it "writes the fixture" do
+        mock(File).open.with_any_args do |*args|
+          args[0].should == "test_dir/fixtures/200_links_id.fixture"
+          args[1].should == "w"
+
+          file_mock = Object.new
+          mock(file_mock).write("CONTENT")
+          args[2].call(file_mock)
+        end
+        mock_route.respond_with(200, "CONTENT")
+      end
+
+      it "generates a description" do
+        mock(File).open.with_any_args
+        mock_route.respond_with(200, "CONTENT")
+        mock_route.response[200]['description'].should == "200 response for /links/:id"
+      end
+
+      it "generates the fixture path" do
+        mock(File).open.with_any_args
+        mock_route.respond_with(200, "CONTENT")
+        mock_route.response[200]['fixture_path'].should == "test_dir/fixtures/200_links_id.fixture"
+      end
+
+    end
+
+    context "with options" do
+      before do
+        mock(File).open.with_any_args
+        mock_route.respond_with(200, "CONTENT", :description => "description", :default => true)
+      end
+
+      it "uses the supplied description" do
+        mock_route.response[200]['description'].should == "description"
+      end
+
+      it "accepts a default option" do
+        mock_route.response['default']['description'].should == "description"
+        mock_route.response['default']['fixture_path'].should == "test_dir/fixtures/200_links_id.fixture"
+      end
+    end
+  end
+end
